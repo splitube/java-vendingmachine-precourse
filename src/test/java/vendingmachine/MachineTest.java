819eac5df7e03f787da1cff157da0f64239ac359
splitube <voicesplit@daum.net>
2022-12-15 14:53:40 +0900

    end2

--- src/test/java/vendingmachine/MachineTest.java
+++ src/test/java/vendingmachine/MachineTest.java
@@ -1,4 +1,25 @@
+package vendingmachine;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 class MachineTest {
-  
+
+    private Machine machine;
+
+    @BeforeEach
+    void before() {
+        machine = new Machine(1000);
+    }
+
+    @Test
+    void makeRandomCoins() {
+
+    }
+
+    @Test
+    void getRandomAmount() {
+    }
 }
--- src/test/java/view/InputViewTest.java
+++ src/test/java/view/InputViewTest.java
@@ -1,4 +1,18 @@
-import static org.junit.jupiter.api.Assertions.*;
+package view;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 class InputViewTest {
-  
+
+    @DisplayName("보유 금액 입력 기능")
+    @ParameterizedTest
+    @CsvSource({"1000, 1000", "500, 500"})
+    void getHoldingAmount(String input, long expect) {
+        InputView inputView = new InputView();
+        long result = inputView.getHoldingAmount(input);
+        Assertions.assertThat(result).isEqualTo(expect);
+    }
 }
